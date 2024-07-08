159490c35ab3d07e935e11ace32934d80533c98b
Camilo Rincon <123512032+carin91@users.noreply.github.com>
2024-07-08 08:48:15 -0500

    feat: including students in assignees

--- .github/issues/assignees.mjs
+++ .github/issues/assignees.mjs
@@ -1,5 +1,5 @@
 function getAssignees(assignees) {
-  const options = ["CamilaSarmiento1", "luisalejandrosalazar", "carin91"];
+  const options = ["CamilaSarmiento1", "luisalejandrosalazar", "carin91", "Juanmediina", "valentinavargas31"];
   assignees.forEach((assignee) => {
     if (!options.includes(assignee)) {
       throw new Error(`Assignee "${assignee}" not found`);
