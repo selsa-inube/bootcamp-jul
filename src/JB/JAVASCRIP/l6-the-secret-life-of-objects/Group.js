fbde2d76ea20a94d19cda18f2cb286739a2e5ff5
Jeissonleoenlinea <jbarajas@sistemasenlinea.com.co>
2024-11-25 16:06:55 -0500

    l6-the-secret-life-of-objects.md

---
+++ src/JB/JAVASCRIP/l6-the-secret-life-of-objects/Group.js
@@ -0,0 +1,36 @@
+class Group {
+  constructor() {
+    this.members = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.members = this.members.filter((member) => member !== value);
+  }
+
+  has(value) {
+    return this.members.includes(value);
+  }
+
+  static from(iterable) {
+    const group = new this();
+    for (let value of iterable) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([10, 20]);
+
+console.log(group.has(10));
+console.log(group.has(30));
+
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
